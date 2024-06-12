9623e817119bb5f8c94d18b83e4c8e54e41ebe32
Ankit Singh <ankiitsingh21@gmail.com>
2024-06-13 00:51:29 +0530

    Added tweet service with hashtag responding

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const connect = require('./config/database');
 const app = express();
-const TweetRepository=require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+
+
 
 app.listen(3000, async ()=>{
         console.log(`Server Started`);
         await connect();
         console.log(`Mongo db connected`);
-        //const tweet = await Tweet.create({
-        //        content :'second tweet',
-        //        //userEmail : 'ankitsingh@gmail.com'
-        //});
-        //const tweets=await Tweet.find({userEmail:'ankitsingh@gmail.com'});
-        //const TweetRepo = new TweetRepository();
-        //const tweet = await TweetRepo.create({content:'myTweet'});
-        //tweet.comments.push({content:'first comment'});
-        //await tweet.save();
-        ////const tweets = await TweetRepo.get('665d8882151eb4bdfdb11b8a');
-        //const tweet= await TweetRepo.create({content:'Fourth Tweet'});
-        //const comment = await Comment.create({content:'new Comment'});
-        //tweet.comments.push(comment);
-        //await tweet.save();
-        //const tweet= await TweetRepo.getAll(0,0);
-        ////const tweet = await TweetRepo.create({content :'With hooks now'});
-        //console.log(tweet[0].contentWithEmail);
 }); 
---
+++ src/services/tweet-service.js
@@ -0,0 +1,29 @@
+const {TweetRepository, HashtagRepository} = require('../repository/index');
+class TweetService {
+        constructor(){
+                this.tweetRepository = new TweetRepository();
+                this.hashtagRepository = new HashtagRepository();
+        }
+        async create(data){
+                try {
+                        const content = data.content;
+                        let tags= content.match(/#[a-zA-Z0-9]+/g).map((tag)=> tag.substring(1)); //this regex extractx hashtags from strings
+                        const tweet = await this.tweetRepository.create(data);
+                        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+                        let titleOfPresentTags = alreadyPresentTags.map(tags=> tags.title);
+                        let hashtags = tags.filter(tags=>!titleOfPresentTags.includes(tags));
+                        hashtags = hashtags.map(tag => {
+                                return {title: tag ,tweets: [tweet.id]}
+                        });
+                        await this.hashtagRepository.bulkCreate(hashtags);
+                        alreadyPresentTags.forEach((tag)=>{
+                                tag.tweets.push(tweet.id);
+                                tag.save();
+                        })
+                        return tweet;
+                } catch (error) {
+                        console.log(error);
+                }
+        }
+}
+module.exports= TweetService;

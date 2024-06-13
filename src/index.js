200740af5aa2a13c2bda23f940b2da55bb813390
Ankit Singh <ankiitsingh21@gmail.com>
2024-06-14 02:38:23 +0530

    Added routes and Controller and cleaned up the tweet-service.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from'express';
 import {connect} from'./config/database.js';
+import bodyParser from 'body-parser';
+import apiRoutes from './routes/index.js';
 const app = express();
 
-import service from './services/tweet-service.js';
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api',apiRoutes);
+
+app.use(bodyParser.json());
 app.listen(3000, async ()=>{
         console.log(`Server Started`);
         await connect();
         console.log(`Mongo db connected`);
-        let ser = new service();
-        // await ser.create({
-        //         content:"hey #wow",
-        // })
-
 }); 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,33 +1,43 @@
-import {TweetRepository,HashtagRepository} from '../repository/index.js'
-class TweetService {
-        constructor(){
-                this.tweetRepository = new TweetRepository();
-                this.hashtagRepository = new HashtagRepository();
-        }
-        async create(data){
-                try {
-                        const content = data.content;
-                        let tags= content.match(/#[a-zA-Z0-9]+/g)
-                                        .map((tag)=> tag.substring(1))
-                                        .map((tag)=>tag.toLowerCase()); //this regex extractx hashtags from strings
+import { TweetRepository, HashtagRepository } from '../repository/index.js';
 
-                                        
-                        const tweet = await this.tweetRepository.create(data);
-                        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
-                        let titleOfPresentTags = alreadyPresentTags.map(tags=> tags.title);
-                        let hashtags = tags.filter(tags=>!titleOfPresentTags.includes(tags));
-                        hashtags = hashtags.map(tag => {
-                                return {title: tag ,tweets: [tweet.id]}
-                        });
-                        await this.hashtagRepository.bulkCreate(hashtags);
-                        alreadyPresentTags.forEach((tag)=>{
-                                tag.tweets.push(tweet.id);
-                                tag.save();
-                        })
-                        return tweet;
-                } catch (error) {
-                        console.log(error);
-                }
+class TweetService {
+    constructor() {
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
+    }
+
+    async create(data) {
+        try {
+            const content = data.content;
+            let tags = content.match(/#[a-zA-Z0-9]+/g);
+            if (tags) {
+                tags = tags.map((tag) => tag.substring(1)).map((tag) => tag.toLowerCase());
+            } else {
+                tags = [];
+            }
+
+            const tweet = await this.tweetRepository.create(data);
+            let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+            let titleOfPresentTags = alreadyPresentTags.map(tag => tag.title);
+            let hashtags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+
+            hashtags = hashtags.map(tag => {
+                return { title: tag, tweets: [tweet.id] };
+            });
+
+            await this.hashtagRepository.bulkCreate(hashtags);
+
+            alreadyPresentTags.forEach((tag) => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
+
+            return tweet;
+        } catch (error) {
+            console.log(error);
+            throw error;
         }
+    }
 }
+
 export default TweetService;
